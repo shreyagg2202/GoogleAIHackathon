64d8e5c5062679ac7d60752a7cd553fd7a464169
Shrey Aggarwal <shreyagg2202@gmail.com>
2024-10-02 18:11:43 +0530

    fixed errors

--- app.py
+++ app.py
@@ -92,51 +92,31 @@ model = genai.GenerativeModel(
     generation_config=generation_config,
 )
 
-# model = genai.GenerativeModel("gemini-pro")
-chat = model.start_chat(history = st.session_state.history)
+def translate_role_for_streamlit(user_role):
+    if user_role == "model":
+        return "assistant"
+    else:
+        return user_role
+    
+# Initialize chat session in Streamlit if not already present
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = model.start_chat(history=[])
 
-with st.sidebar:
-    if st.button("Clear Chat Window", use_container_width=True, type="primary"):
-        st.session_state.history = []
-        st.rerun()
-
-for message in chat.history:
-    role ="assistant" if message.role == 'model' else message.role
-    with st.chat_message(role):
+# Display the chat history
+for message in st.session_state.chat_session.history:
+    with st.chat_message(translate_role_for_streamlit(message.role)):
         st.markdown(message.parts[0].text)
 
-while True:
-    if prompt := st.chat_input(""):
-        prompt = prompt.replace('\n', ' \n')
-        prompt = make_prompt(prompt)
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            message_placeholder.markdown("Thinking...")
-            try:
-                full_response = ""
-                for chunk in chat.send_message(prompt, stream=True):
-                    word_count = 0
-                    random_int = random.randint(5,10)
-                    for word in chunk.text:
-                        full_response+=word
-                        word_count+=1
-                        if word_count == random_int:
-                            time.sleep(0.05)
-                            message_placeholder.markdown(full_response + "_")
-                            word_count = 0
-                            random_int = random.randint(5,10)
-                message_placeholder.markdown(full_response)
-            except genai.types.generation_types.BlockedPromptException as e:
-                st.exception(e)
-            except Exception as e:
-                st.exception(e)
-            st.session_state.history = chat.history
+# Input field for user's message
+user_prompt = st.chat_input("Ask Gemini-Pro...")
+if user_prompt:
+    # Add user's message to chat and display it
+    st.chat_message("user").markdown(user_prompt)
 
+    # Send user's message to Gemini-Pro and get the response
+    prompt = make_prompt(user_prompt)
+    gemini_response = st.session_state.chat_session.send_message(prompt)
 
-
-
-
-
-
+    # Display Gemini-Pro's response
+    with st.chat_message("assistant"):
+        st.markdown(gemini_response.text)
