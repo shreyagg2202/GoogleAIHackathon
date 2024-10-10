df5befd43cb496f0d4092ebe4a30713d47a22d56
Shrey Aggarwal <shreyagg2202@gmail.com>
2024-10-11 00:15:31 +0530

    added more keywords

--- semantic_search.py
+++ semantic_search.py
@@ -7,15 +7,22 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Initialize the model and keyphrase embeddings when the module is imported
 Semantic_Search_model = SentenceTransformer('all-mpnet-base-v2')
 
-threshold = 0.75
+threshold = 0.65
 Keyphrases = [
-    "I want to select this policy", "I want to go ahead with this policy",
+    "I want to select this policy", "I want to go ahead with this policy", 
     "I want to choose this policy", "Let's proceed with this policy", "Sign me up for this policy", "I'd like to buy this policy",
     "This policy suits my needs", "This is my preferred policy",
     "I want to purchase this policy", "I'm ready to enroll in this policy",
     "This policy works for me", "I have decided on this policy", "Please proceed with this policy",
     "I want to move forward with this policy", "This is the policy I'd like", "Enroll me in this policy",
     "I am interested in purchasing this policy",
+    "I want to select this plan", "I want to go ahead with this plan", 
+    "I want to choose this plan", "Let's proceed with this plan", "Sign me up for this plan", "I'd like to buy this plan",
+    "This plan suits my needs", "This is my preferred plan",
+    "I want to purchase this plan", "I'm ready to enroll in this plan",
+    "This plan works for me", "I have decided on this plan", "Please proceed with this plan",
+    "I want to move forward with this plan", "This is the plan I'd like", "Enroll me in this plan",
+    "I am interested in purchasing this plan"
 ]
 
 Keyphrase_embeddings = Semantic_Search_model.encode(Keyphrases)
